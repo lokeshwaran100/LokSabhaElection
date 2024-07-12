401d4566b66a2097cdea9054ae50d365096b9d27
Lokeshwaran Boopathy <lokeshwaran100@gmail.com>
2024-07-12 21:45:49 +0530

    Revealed age and pincode

---
+++ contracts/contracts/LokSabhaElection.sol
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.19;
+
+import "@anon-aadhaar/contracts/interfaces/IAnonAadhaar.sol";
+// import "@anon-aadhaar/contracts/interfaces/IAnonAadhaarVote.sol";
+
+contract LokSabhaElection {
+    struct Candidate {
+        string name;
+        string party;
+        uint[] pincodes;
+        uint voteCount;
+    }
+
+    string public electionName;
+    address public anonAadhaarVerifierAddr;
+
+    // List of candidates
+    Candidate[] public candidates;
+
+    // Mapping to track if a userNullifier has already voted
+    mapping(uint256 => bool) public hasVoted;
+
+    // Constructor to initialize election and candidates
+    constructor(
+        string memory _electionName,
+        address _verifierAddr,
+        string[] memory candidateNames,
+        string[] memory parties,
+        uint[][] memory candidatePincodes
+    ) {
+        anonAadhaarVerifierAddr = _verifierAddr;
+        electionName = _electionName;
+        require(
+            candidateNames.length == parties.length && candidateNames.length == candidatePincodes.length,
+            "[LokSabhaElection]: Input arrays must have the same length"
+        );
+        for (uint256 i = 0; i < candidateNames.length; i++) {
+            candidates.push(Candidate(candidateNames[i], parties[i], candidatePincodes[i], 0));
+        }
+    }
+
+    /// @dev Convert an address to uint256, used to check against signal.
+    /// @param _addr: msg.sender address.
+    /// @return Address msg.sender"s address in uint256
+    function addressToUint256(address _addr) private pure returns (uint256) {
+        return uint256(uint160(_addr));
+    }
+
+    /// @dev Register a vote in the contract.
+    /// @param candidateIndex: Index of the candidate you want to vote for.
+    /// @param nullifierSeed: Nullifier Seed used while generating the proof.
+    /// @param nullifier: Nullifier for the citizen"s Aadhaar data.
+    /// @param timestamp: Timestamp of when the QR code was signed.
+    /// @param signal: signal used while generating the proof, should be equal to msg.sender.
+    /// @param revealArray: Array of the values used as input for the proof generation (equal to [0, 0, 0, 0] if no field reveal were asked).
+    /// @param groth16Proof: SNARK Groth16 proof.
+    function voteForCandidate(
+        uint256 candidateIndex,
+        uint nullifierSeed,
+        uint nullifier,
+        uint timestamp,
+        uint signal,
+        uint[4] memory revealArray, 
+        uint[8] memory groth16Proof
+    ) public {
+        require(
+            candidateIndex < candidates.length,
+            "[LokSabhaElection]: Invalid candidate index"
+        );
+        require(
+            addressToUint256(msg.sender) == signal,
+            "[LokSabhaElection]: Wrong citizen signal sent."
+        );
+        // require(
+        //     isLessThan3HoursAgo(timestamp) == true,
+        //     "[LokSabhaElection]: Proof must be generated with Aadhaar data generated less than 3 hours ago."
+        // );
+        require(
+            IAnonAadhaar(anonAadhaarVerifierAddr).verifyAnonAadhaarProof(
+                nullifierSeed, // nullifier seed
+                nullifier,
+                timestamp,
+                signal,
+                revealArray,
+                groth16Proof
+            ) == true,
+            "[LokSabhaElection]: Proof sent is not valid."
+        );
+        // Check that citizen hasn"t already voted
+        require(
+            !hasVoted[nullifier],
+            "[LokSabhaElection]: Citizen has already voted"
+        );
+
+        // Check that the voter belongs to the candidate"s pincode
+        bool isEligible = false;
+        for (uint256 i = 0; i < candidates[candidateIndex].pincodes.length; i++) {
+            if (candidates[candidateIndex].pincodes[i] == revealArray[2]) {
+                isEligible = true;
+                break;
+            }
+        }
+        require(isEligible, "[LokSabhaElection]: Voter does not belong to candidate's pincode");
+
+        candidates[candidateIndex].voteCount++;
+        hasVoted[nullifier] = true;
+
+        // emit Voted(msg.sender, candidateIndex);
+    }
+
+    // Function to get the total number of candidates
+    function getCandidateCount() public view returns (uint256) {
+        return candidates.length;
+    }
+
+    // Function to get candidate information by index
+    function getCandidate(
+        uint256 candidateIndex
+    ) public view returns (string memory, string memory, uint256) {
+        require(
+            candidateIndex < candidates.length,
+            "[LokSabhaElection]: Invalid candidate index"
+        );
+
+        Candidate memory candidate = candidates[candidateIndex];
+        return (candidate.name, candidate.party, candidate.voteCount);
+    }
+
+    // Function to check if a citizen has already voted
+    function checkVoted(uint256 _nullifier) public view returns (bool) {
+        return hasVoted[_nullifier];
+    }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,96 @@
+/* eslint-disable react/no-unescaped-entities */
+import { LaunchProveModal, useAnonAadhaar } from "@anon-aadhaar/react";
+import { useEffect, useContext } from "react";
+import { useRouter } from "next/router";
+import { useAccount } from "wagmi";
+import { AppContext } from "./_app";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+
+// This is a trick to enable having both modes in under the same page.
+// This could be removed and only the <LaunchProveModal /> could be displayed.
+const LaunchMode = ({
+  isTest,
+  setIsTestMode,
+  address,
+}: {
+  isTest: boolean;
+  setIsTestMode: (isTest: boolean) => void;
+  address: string;
+}) => {
+  return (
+    <span onClick={() => setIsTestMode(isTest)}>
+      <LaunchProveModal
+        nullifierSeed={Math.floor(Math.random() * 1983248)}
+        signal={address}
+        fieldsToReveal={["revealAgeAbove18", "revealPinCode"]}
+        buttonStyle={{
+          borderRadius: "8px",
+          border: "solid",
+          borderWidth: "1px",
+          boxShadow: "none",
+          fontWeight: 500,
+          borderColor: "#009A08",
+          color: "#009A08",
+          fontFamily: "rajdhani",
+        }}
+        buttonTitle={isTest ? "USE TEST CREDENTIALS" : "USE REAL CREDENTIALS"}
+        useTestAadhaar={isTest}
+      />
+    </span>
+  );
+};
+
+export default function Home() {
+  const [anonAadhaar] = useAnonAadhaar();
+  const { setIsTestMode } = useContext(AppContext);
+  const { isConnected, address } = useAccount();
+  const { open } = useWeb3Modal();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (anonAadhaar.status === "logged-in") {
+      router.push("./vote");
+    }
+  }, [anonAadhaar, router]);
+
+  return (
+    <>
+      <main className="flex flex-col min-h-[75vh] mx-auto justify-center items-center w-full p-4">
+        <div className="max-w-4xl w-full">
+          <h6 className="text-[36px] font-rajdhani font-medium leading-none">
+            ANON AADHAAR
+          </h6>
+          <h2 className="text-[90px] font-rajdhani font-medium leading-none">
+            EXAMPLE VOTING APP
+          </h2>
+          <div className="text-md mt-4 mb-8 text-[#717686]">
+            This process ensures anonymity by utilizing the Aadhaar secure QR
+            code (present on e-Aadhaar and the printed Aadhaar letter) which
+            preserves the confidentiality of the Aadhaar number.
+          </div>
+
+          <div className="flex w-full gap-8 mb-8">
+            {isConnected ? (
+              <div>
+                <div className="flex gap-4 place-content-center">
+                  <LaunchMode
+                    isTest={true}
+                    setIsTestMode={setIsTestMode}
+                    address={address as string}
+                  />
+                </div>
+              </div>
+            ) : (
+              <button
+                className="bg-[#009A08] rounded-lg text-white px-6 py-1 font-rajdhani font-medium"
+                onClick={() => open()}
+              >
+                CONNECT WALLET
+              </button>
+            )}
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
